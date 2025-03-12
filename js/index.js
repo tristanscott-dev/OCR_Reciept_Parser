1ee3839772e08dbbc4590a05368600b1dde9f9ee
tristanscott.dev <tristanscott.dev@gmail.com>
2025-03-12 19:25:07 +1300

    added basixc tesseract

--- js/index.js
+++ js/index.js
@@ -2,19 +2,29 @@
 
 $(document).ready(function(){
 
+    //https://github.com/naptha/tesseract.js/blob/master/docs/examples.md
+    //Tesseract.createWorker('eng');
+
+    (async () => {
+        const worker = await Tesseract.createWorker('eng');
+        const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
+        console.log(text);
+        await worker.terminate();
+    })();
+
     $('#submit-button').attr('disabled',true);
 
     console.log($('#formFile'));
 
-    $('#formFile').addEventListener("input", () =>{
+    // $('#formFile').addEventListener("input", () =>{
 
-        console.log("On input");
+    //     console.log("On input");
 
-        if($(this).val().length !=0)
-            $('#submit-button').attr('disabled', false);            
-        else
-            $('#submit-button').attr('disabled',true);
-    });
+    //     if($(this).val().length !=0)
+    //         $('#submit-button').attr('disabled', false);            
+    //     else
+    //         $('#submit-button').attr('disabled',true);
+    // });
 
     $('#formFile').keyup(function(){
 
@@ -30,10 +40,32 @@ $(document).ready(function(){
 function onFileSelected(){
     console.log("File Selected");
 
+    console.log($("#formFile").prop('files')[0]);
+
     if($('#formFile').val().length !=0)
         $('#submit-button').attr('disabled', false);            
     else
         $('#submit-button').attr('disabled',true);
+
+        var fr = new FileReader();
+        //fr.onload = createImage;   // onload fires after reading is complete
+        fr.readAsDataURL($("#formFile").prop('files')[0]);  
+    
+        var canvas = document.getElementById("preview-canvas");
+        canvas.style.display = 'block';
+    var ctx = canvas.getContext("2d");
+
+    ctx.drawImage(fr.result, 0,0, canvas.width, canvas.height  );  
+
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    (async () => {
+        const worker = await Tesseract.createWorker('eng');
+        const { data: { text } } = await worker.recognize($("#formFile").prop('files')[0]);
+        console.log(text);
+        await worker.terminate();
+    })();
+
 }
 
 function onFileSubmitPress(){
