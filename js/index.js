4902427ba322198f16b3a79ac150a20957c3d601
tristanscott.dev <tristanscott.dev@gmail.com>
2025-03-21 18:22:52 +1300

    Working through scroll

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+const SCROLL_SENSITIVITY = 0.0001;
+const MAX_ZOOM = 2;
+const MIN_ZOOM = 0.4;
+
 let pan = { x: window.innerWidth/2, y: window.innerHeight/2 }
 let zoom = 1;
 let isPanning = false;
@@ -10,6 +14,7 @@ var canvas;
 
 var image;
 
+// https://phrogz.net/tmp/canvas_zoom_to_cursor.html
 
 // Gets the relevant location from a mouse or single touch event
 function getEventLocation(e)
@@ -36,8 +41,74 @@ $(document).ready(function(){
     //     await worker.terminate();
     // })();
 
+
+
     canvas = document.getElementById("preview-canvas");
     ctx = canvas.getContext("2d");
+
+    canvas.addEventListener('DOMMouseScroll',handleMouseScroll,false);
+    canvas.addEventListener('mousewheel',handleMouseScroll,false);
+
+
+    canvas.addEventListener('mousedown', (e) => {
+
+        // isPanning = true;
+
+        // panStart.x = getEventLocation(e).x / zoom - pan.x
+        // panStart.y = getEventLocation(e).y / zoom - pan.y
+
+    })
+    // canvas.addEventListener('touchstart', (e) => handleTouch(e, onPointerDown))
+    canvas.addEventListener('mouseup', (e) => {
+
+        // isPanning = false;
+    })
+    // canvas.addEventListener('touchend',  (e) => handleTouch(e, onPointerUp))
+    canvas.addEventListener('mousemove', (e) => {
+
+        // if (isPanning)
+        // {
+        //     pan.x = getEventLocation(e).x / zoom - panStart.x
+        //     pan.y = getEventLocation(e).y / zoom - panStart.y
+        // }
+
+    });
+
+    // canvas.addEventListener('touchmove', (e) => handleTouch(e, onPointerMove))
+    // canvas.addEventListener( 'wheel', (e) => {
+
+    //     //console.log("canvas wheel " , e.deltaY );
+
+    //     // if (!isPanning)
+    //     // {
+    //     //     return;
+    //     // }
+
+    //    zoom +=  e.deltaY * SCROLL_SENSITIVITY;
+    //    console.log("canvas new zoom " , zoom );
+
+    //    if( zoom >= MAX_ZOOM){
+    //         zoom = MAX_ZOOM;
+    //         return;
+    //    }
+
+    //    if( zoom <= MIN_ZOOM){
+    //         zoom = MIN_ZOOM;
+    //         return;
+    //     }
+
+
+    //    //zoom = Math.min( zoom, MAX_ZOOM )
+    //  //  zoom = Math.max( zoom, MIN_ZOOM )
+       
+
+    
+
+    //    // drawCanvas();
+
+    //    // e.preventDefault();
+    // })
+
 
 
     const fileInput = document.getElementById("formFile");
@@ -71,108 +142,24 @@ $(document).ready(function(){
         canvas.style.display = 'block';
     
 
-        canvas.addEventListener('mousedown', (e) => {
 
-            isPanning = true;
-
-            panStart.x = getEventLocation(e).x / zoom - cameraOffset.x
-            panStart.y = getEventLocation(e).y / zoom - cameraOffset.y
-
-        })
-        // canvas.addEventListener('touchstart', (e) => handleTouch(e, onPointerDown))
-        canvas.addEventListener('mouseup', (e) => {
-
-            isPanning = false;
-        })
-        // canvas.addEventListener('touchend',  (e) => handleTouch(e, onPointerUp))
-        canvas.addEventListener('mousemove', (e) => {
-
-            if (isDragging)
-            {
-                pan.x = getEventLocation(e).x / zoom - panStart.x
-                pan.y = getEventLocation(e).y / zoom - panStart.y
-            }
-
-        });
-
-        // canvas.addEventListener('touchmove', (e) => handleTouch(e, onPointerMove))
-        canvas.addEventListener( 'wheel', (e) => {
-
-            if (!isDragging)
-            {
-                return;
-            }
-
-            zoom +=  e.deltaY * SCROLL_SENSITIVITY;
-
-           zoom = Math.min( zoom, MAX_ZOOM )
-           zoom = Math.max( zoom, MIN_ZOOM )
-           
-
-            
-
-            console.log("canvas wheel " , zoom);
-
-            drawCanvas();
-        })
     
-        console.log("canvas width " , canvas.width);
-        console.log("canvas height " , canvas.height);
-        // updateCanvasSize();
+        // console.log("canvas width " , canvas.width);
+        // console.log("canvas height " , canvas.height);
+        // // updateCanvasSize();
 
-        var canvasMeasuredWidth = canvas.getBoundingClientRect().width;
-        var canvasMeasuredHeight = canvas.getBoundingClientRect().height;
+        // var canvasMeasuredWidth = canvas.getBoundingClientRect().width;
+        // var canvasMeasuredHeight = canvas.getBoundingClientRect().height;
 
-        console.log("canvas canvasMeasuredWidth: " , canvasMeasuredWidth);
+        // console.log("canvas canvasMeasuredWidth: " , canvasMeasuredWidth);
 
-        console.log("canvas canvasMeasuredHeight: " , canvasMeasuredHeight);
-
-        var imageDrawWidth;
-        var imageDrawHeight;
-
-        var widthIsShortestSideLength = canvasMeasuredWidth < canvasMeasuredHeight;
-        if(widthIsShortestSideLength){
-
-            // if the width is the shortest we need to use it as our max and 
-            // resize the height accordingly
-            imageDrawWidth = image.width > canvasMeasuredWidth ? canvasMeasuredWidth : image.width;
-            imageDrawHeight = imageDrawWidth * imageWidthAspectRatio ;
-
-            console.log("widthIsShortestSideLength " , imageDrawWidth);
-
-            console.log("widthIsShortestSideLength imageDrawWidth " , imageDrawWidth);
-            console.log("widthIsShortestSideLength imageDrawHeight " , imageDrawHeight);
-
-        }else{
-
-
-           // console.log("widthIsShortestSideLength " , imageDrawWidth);
-
-            // the height is the shortet and we need to use its ratio when drawing
-            imageDrawHeight = image.height > canvasMeasuredHeight ? canvasMeasuredHeight : image.height;
-            imageDrawWidth = imageDrawHeight * imageHeightAspectRatio ;
-
-
-            console.log("widthIsShortestSideLength NOT : " , imageDrawWidth);
-
-            console.log("widthIsShortestSideLength NOT :  imageDrawWidth " , imageDrawWidth);
-            console.log("widthIsShortestSideLength NOT :  imageDrawHeight " , imageDrawHeight);
-        }
+        // console.log("canvas canvasMeasuredHeight: " , canvasMeasuredHeight);
 
 
 
-        // initCanvasData().then(() => {
-            
-        //     // bindCanvasClickHandler(ctx);
-        //     draw();
+        drawCanvas();
 
-        //     notifyCanvasReady();
-
-        // });
-
-        // Ready, set, go
-        //draw(canvas, ctx);
-
+    
 
     });
 
@@ -181,6 +168,44 @@ $(document).ready(function(){
 
 
 });
+
+function doZoom(clicks){
+   // var pt = ctx.transformedPoint(lastX,lastY);
+  //  ctx.translate(pt.x,pt.y);
+    //var factor = Math.pow(scaleFactor,clicks);
+    ctx.scale(zoom,zoom);
+   // ctx.translate(-pt.x,-pt.y);
+    drawCanvas();
+}
+
+function handleMouseScroll(e){
+
+
+    zoom +=  e.deltaY * SCROLL_SENSITIVITY;
+
+    // if( zoom >= MAX_ZOOM){
+    //     zoom = MAX_ZOOM;
+    //     return;
+    // }
+
+    // if( zoom <= MIN_ZOOM){
+    //     zoom = MIN_ZOOM;
+    //     return;
+    // }
+
+
+    console.log("handleMouseScroll zoom ", zoom);
+    if (zoom) {
+
+        drawCanvas();
+    }
+    
+   
+
+    return e.preventDefault() && false;
+
+
+}
 
 function drawCanvas(){
 
@@ -193,12 +218,13 @@ function drawCanvas(){
     var centerShift_x = ( canvas.width - image.width * ratio ) / 2;
     var centerShift_y = ( canvas.height - image.height * ratio ) / 2;  
 
-   
-    // ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx.clearRect(0,0,canvas.width, canvas.height);
+    console.log("dra canvas applying zoom " , zoom);
+
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    //ctx.clearRect(0,0,canvas.width, canvas.height);
 
     // ctx.translate( canvas.width / 2, canvas.height / 2 )
-    // ctx.scale(zoom, zoom)
+    ctx.scale(zoom, zoom)
     // ctx.translate( -canvas.width / 2 + cameraOffset.x, -canvas.height / 2 + cameraOffset.y )
 
     ctx.drawImage(image,0, 0, image.width , image.height, 0, 0,  image.width * ratio, image.height * ratio);  
@@ -258,28 +284,28 @@ function onFileSelected(event){
  
 
 
-    (async () => {
-        const worker = await Tesseract.createWorker('eng');
-        const { data: { text } } = await worker.recognize($("#formFile").prop('files')[0]);
-        console.log(text);
-        await worker.terminate();
-    })();
+    // (async () => {
+    //     const worker = await Tesseract.createWorker('eng');
+    //     const { data: { text } } = await worker.recognize($("#formFile").prop('files')[0]);
+    //     console.log(text);
+    //     await worker.terminate();
+    // })();
 
 }
 
 
-function updateCanvasSize(image){
+// function updateCanvasSize(image){
 
-    var parentContainer = document.getElementById("receipt-container");
-    var canvas =  document.getElementById("preview-canvas");
+//     var parentContainer = document.getElementById("receipt-container");
+//     var canvas =  document.getElementById("preview-canvas");
 
-    var canvasWidth =  parentContainer.width < 460 ? parentContainer.width : 460;
-   // canvas.width = 120; //;
+//     var canvasWidth =  parentContainer.width < 460 ? parentContainer.width : 460;
+//    // canvas.width = 120; //;
 
-   // canvas.height = canvas.width * ( image.height / image.width) ;
+//    // canvas.height = canvas.width * ( image.height / image.width) ;
 
-    console.log("updateCanvasSize parentContainer.width: " + parentContainer.getBoundingClientRect().width);
-    // console.log("updateCanvasSize parentContainer.width: " + parentContainer.width);
+//     console.log("updateCanvasSize parentContainer.width: " + parentContainer.getBoundingClientRect().width);
+//     // console.log("updateCanvasSize parentContainer.width: " + parentContainer.width);
 
-}
+// }
 
